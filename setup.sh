1f4bc99fd7849b2f2cf802d6cd23063f742af5af
Nate Wise <nathan.wise@wealthbox.com>
2025-01-02 11:28:26 -0500

    fixing perms

--- setup.sh
+++ setup.sh
@@ -6,3 +6,15 @@ rm -rf ~/.config
 ln -s ~/dotfiles/.oh-my-zsh ~/.oh-my-zsh
 ln -s ~/dotfiles/.zshenv ~/.zshenv
 ln -s ~/dotfiles/.config ~/.config
+
+export CACHE_DIR=/usr/local/cache
+
+mkdir -p /usr/local/bundle
+chmod -R 777 /usr/local/bundle
+
+mkdir -p /usr/local/lib/ruby
+chmod -R 777 /usr/local/lib/ruby
+
+chmod -R 777 /usr/local/bin
+chmod -R 777 $CACHE_DIR
+
