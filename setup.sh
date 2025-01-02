8cc587742a319ef46331811446f54b9226f6dd57
Nate Wise <nathan.wise@wealthbox.com>
2025-01-02 13:35:39 -0500

    building off of crm-web ruby image

--- setup.sh
+++ setup.sh
@@ -1,14 +1,13 @@
 #!/bin/zsh
 
+sh -c "$(curl https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
+
 pwd
 rm -rf ~/.oh-my-zsh
 rm -rf ~/.config
 ln -s ~/dotfiles/.oh-my-zsh ~/.oh-my-zsh
 ln -s ~/dotfiles/.zshenv ~/.zshenv
 ln -s ~/dotfiles/.config ~/.config
-
-sudo apt-get update
-sudo apt-get install -y libpq-dev shared-mime-info pkg-config libgpgme-dev
 
 export CACHE_DIR=/usr/local/cache
 
